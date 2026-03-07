822582a696e75c071a6cb69a6e3c5d7e45239672
Pete Colasacco <pcola625@gmail.com>
2026-03-07 14:36:03 -0800

    add broken tests

---
+++ src/tests/getAPIKey.test.ts
@@ -0,0 +1,34 @@
+import {getAPIKey} from "../api/auth.js";
+/*
+export function getAPIKey(headers: IncomingHttpHeaders): string | null {
+  const authHeader = headers["authorization"];
+  if (!authHeader) {
+    return null;
+  }
+
+  const splitAuth = authHeader.split(" ");
+  if (splitAuth.length < 2 || splitAuth[0] !== "ApiKey") {
+    return null;
+  }
+
+  return splitAuth[1];
+}
+*/
+
+import { describe, expect, test } from "vitest";
+
+
+describe("API tests", () => {
+	test("basic", ()=> {
+		const fakeheader: IncomingHttpHeaders  = {
+			'authorization' : 'Basic: losername:assword' 
+			};
+		expect(getAPIKey(fakeheader)).toBe(null);
+	});
+	test("actual",  ()=> {
+		const realheader: IncomingHttpHeaders = {
+			'authorization' : 'ApiKey zippideddoodah'
+			};
+		expect(getAPIKey(realheader)).toBe("zippededdoodah");
+	});
+});
